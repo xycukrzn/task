2fabccef6c23c3f8153afa25ed8ec4faaeff3fce
xycukrzn <xyc_uk@icloud.com>
2023-07-05 21:47:56 +0300

    первое задание

---
+++ NikolayDorozhkin1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+if (a > b) {
+    Console.WriteLine("Первое число меньше второго");
+}
+else {
+    Console.WriteLine("Второе число меньше первого");
+}
