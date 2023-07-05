1e1ab7b0c734381edb09320f84baa8dc91b7188f
xycukrzn <xyc_uk@icloud.com>
2023-07-05 22:39:30 +0300

    Задание2

--- NikolayDorozhkin2/Program.cs
+++ NikolayDorozhkin2/Program.cs
@@ -3,16 +3,16 @@ int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int b = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите третье число ");
-int с = int.Parse(Console.ReadLine());
-if (a > b && a > c ) {
+int c = int.Parse(Console.ReadLine());
+if ((a > b) && (a > c )) {
     Console.WriteLine("Первое число самое большое");
 }
-if (a > b && a < c ) {
+if ((a > b) && (a < c )) {
     Console.WriteLine("Второе число самое большое");
 }
-if  (a < b && a < c ) {
+if  ((a < b) && (a < c )) {
     Console.WriteLine("Третье число самое большое");
 }
 else {
-    Console.WriteLine("Хм");
+    Console.WriteLine("Хм, попробуй еще");
 }
