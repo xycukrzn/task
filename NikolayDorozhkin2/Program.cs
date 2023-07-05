27c1711a8076cc1e0635483b0d3b0f3678d4018e
xycukrzn <xyc_uk@icloud.com>
2023-07-05 22:28:07 +0300

    2task

---
+++ NikolayDorozhkin2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int с = int.Parse(Console.ReadLine());
+if (a > b && a > c ) {
+    Console.WriteLine("Первое число самое большое");
+}
+if (a > b && a < c ) {
+    Console.WriteLine("Второе число самое большое");
+}
+if  (a < b && a < c ) {
+    Console.WriteLine("Третье число самое большое");
+}
+else {
+    Console.WriteLine("Хм");
+}
