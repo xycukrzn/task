7fff99374019f35a292de94558bc0327eac929de
xycukrzn <xyc_uk@icloud.com>
2023-07-06 21:25:07 +0300

    task3

---
+++ NikolayDorozhkin3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine();
+if (a / a == 1) {
+    Console.WriteLine("Число-четное");
+}
+else {
+    Console.WriteLine("Число нечетное");
+}
