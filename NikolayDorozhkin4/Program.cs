88127ea50412d583389d41ee56972dcee999727b
xycukrzn <xyc_uk@icloud.com>
2023-07-06 23:23:43 +0300

    Task4

---
+++ NikolayDorozhkin4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N:");
+        int N = int.Parse(Console.ReadLine());
+        Console.WriteLine("Чётные числа от 1 до " + N + ":");
+        for (int i = 2; i <= N; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+
+
