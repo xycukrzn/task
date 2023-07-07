9b2180148adb1ad0725a3708cec625ad16cf695b
xycukrzn <xyc_uk@icloud.com>
2023-07-07 20:18:03 +0300

    method

--- NikolayDorozhkin4/Program.cs
+++ NikolayDorozhkin4/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Введите число N:");
         int N = int.Parse(Console.ReadLine());
         Console.WriteLine("Чётные числа от 1 до " + N + ":");
-        for (int i = 2; i <= N; i += 2)
-        {
-            Console.WriteLine(i);
+      {
+             for (int i = 2; i <= N; i += 2)
+         Console.WriteLine(i);
         }
 
 
