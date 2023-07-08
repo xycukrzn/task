26b3e0652f1eb06acef60c87abc5955669d99467
xycukrzn <xyc_uk@icloud.com>
2023-07-08 12:00:23 +0300

    трехзначное чило без 2 цифры

---
+++ Method2C#/Program.cs
@@ -0,0 +1,11 @@
+﻿        Random random = new Random();
+        int randomNumber = random.Next(100, 1000);
+        Console.WriteLine("Сгенерированное трехзначное число: " + randomNumber);
+
+        int firstDigit = randomNumber / 100;    // Получаем первую цифру числа
+        int thirdDigit = randomNumber % 10;     // Получаем третью цифру числа
+
+        int newNumber = firstDigit * 10 + thirdDigit;   // Формируем новое число без второй цифры
+
+        Console.WriteLine("Число после удаления второй цифры: " + newNumber);
+    
