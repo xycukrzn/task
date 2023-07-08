4172889e9172c50c949e0a40610c3f0cd4440b48
xycukrzn <xyc_uk@icloud.com>
2023-07-08 12:37:47 +0300

    кратное

--- Method2C#/Program.cs
+++ Method2C#/Program.cs
@@ -1,11 +1,45 @@
-﻿        Random random = new Random();
-        int randomNumber = random.Next(100, 1000);
-        Console.WriteLine("Сгенерированное трехзначное число: " + randomNumber);
+﻿// Random random = new Random();
+// int randomNumber = random.Next(100, 1000);
+// Console.WriteLine("Сгенерированное трехзначное число: " + randomNumber);
 
-        int firstDigit = randomNumber / 100;    // Получаем первую цифру числа
-        int thirdDigit = randomNumber % 10;     // Получаем третью цифру числа
+// int firstDigit = randomNumber / 100;    // Получаем первую цифру числа
+// int thirdDigit = randomNumber % 10;     // Получаем третью цифру числа
 
-        int newNumber = firstDigit * 10 + thirdDigit;   // Формируем новое число без второй цифры
+// int newNumber = firstDigit * 10 + thirdDigit;   // Формируем новое число без второй цифры
 
-        Console.WriteLine("Число после удаления второй цифры: " + newNumber);
+// Console.WriteLine("Число после удаления второй цифры: " + newNumber);
     
+
+//  Console.WriteLine("Введите первое число:");
+//         int number1 = int.Parse(Console.ReadLine());
+
+//         Console.WriteLine("Введите второе число:");
+//         int number2 = int.Parse(Console.ReadLine());
+
+//         if (number2 % number1 == 0)
+//         {
+//             Console.WriteLine(number2 + " является кратным числу " + number1);
+//         }
+//         else
+//         {
+//             int remainder = number2 % number1;
+//             Console.WriteLine(number2 + " не является кратным числу " + number1);
+//             Console.WriteLine("Остаток от деления: " + remainder);
+//         }
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите первое число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number2 % number1 == 0)
+{
+    Console.WriteLine("Число" + number2 + "является кратным числу " + number2;
+}
+else
+{
+    int reminder = number2 % number1;
+    Console.WriteLine("Число" + number2 + " не является кратным числу " + number1);
+    Console.WriteLine("Остаток от деления " + reminder);
+}
