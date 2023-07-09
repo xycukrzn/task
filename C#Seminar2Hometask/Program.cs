f835398f8ccc4ecfbdef44157f96e98571f4cacc
xycukrzn <xyc_uk@icloud.com>
2023-07-09 12:21:09 +0300

    все задания решены 2 семинар

--- C#Seminar2Hometask/Program.cs
+++ C#Seminar2Hometask/Program.cs
@@ -13,15 +13,32 @@
 // Задача 13
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-        Console.WriteLine("Введите число:");
-        string number = Console.ReadLine(); // Хранит последовательность символов в переменной "number"
+// Console.WriteLine("Введите число:");
+// string number = Console.ReadLine(); // Хранит последовательность символов в переменной "number", "string" - текстовая строка
 
-        if (number.Length < 3)
-        {
-            Console.WriteLine("Третьей цифры нет");
-        }
-        else
-        {
-            char thirdDigit = number[2]; // Хранит один единственный "третий" символ в переменной "thirdDigit"
-            Console.WriteLine("Третья цифра числа: " + thirdDigit);
-        }
+// if (number.Length < 3)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+//     char thirdDigit = number[2]; // Хранит один единственный "третий" символ в переменной "thirdDigit"
+//     Console.WriteLine("Третья цифра числа: " + thirdDigit);
+// }
+
+
+
+// Задача 15 
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру дня недели (1-7):");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 6 || day == 7) // если цифра дня это 6 или 7
+{
+    Console.WriteLine("Это выходной день.");
+}
+else
+{
+    Console.WriteLine("Это рабочий день.");
+}
