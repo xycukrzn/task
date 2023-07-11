b477f583ca56e116ee9206a9e38a9cf781a4e5cd
xycukrzn <xyc_uk@icloud.com>
2023-07-11 21:56:46 +0300

    seminar3

--- C#Seminar2Hometask/Program.cs
+++ C#Seminar2Hometask/Program.cs
@@ -28,17 +28,104 @@
 
 
 
+// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// // или сообщает, что третьей цифры нет.
+
+// string mess = "Введите число: ";
+// int number1 = Prompt(mess);
+// int number2 = Prompt("Введите пятизначное число");
+
+// int result1 = FindThirdNumber(number1);
+// int result2 = FindThirdNumber(number2);
+
+// GetResult(result1);
+// GetResult(result2);
+
+
+
+// void GetResult(int res)
+// {
+//     if (res == -1)
+//     {
+//         Console.WriteLine("Такой цифры нет! ");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Третья цифра числа {res}");
+//     }
+// }
+
+
+
+// int Prompt(string message)
+// {
+//     Console.Clear();
+//     Console.WriteLine(message);
+//     int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//     return num;
+// }
+
+// int FindThirdNumber(int num)
+// {
+//     if (num < 100)
+//     {
+//         return -1;
+
+//     }
+//     else
+//     {
+//         while (num >= 1000)
+//         {
+//             num = num / 10;
+//         }
+//         int thirdNumber = num % 10;
+//         return thirdNumber;
+//     }
+// }
+
+
+
 // Задача 15 
 // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.WriteLine("Введите цифру дня недели (1-7):");
-int day = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите цифру дня недели (1-7):");
+// int day = int.Parse(Console.ReadLine());
 
-if (day == 6 || day == 7) // если цифра дня это 6 или 7
+// if (day == 6 || day == 7) // если цифра дня это 6 или 7
+// {
+//     Console.WriteLine("Это выходной день.");
+// }
+// else
+// {
+//     Console.WriteLine("Это рабочий день.");
+// }
+
+
+
+Console.WriteLine("Введите координату X:");
+double x = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y:");
+double y = double.Parse(Console.ReadLine());
+
+int quadrant;
+
+if (x > 0 && y > 0)
 {
-    Console.WriteLine("Это выходной день.");
+quadrant = 1;
+}
+else if (x < 0 && y > 0)
+{
+     quadrant = 2;
+}
+else if (x < 0 && y < 0)
+{
+    quadrant = 3;
 }
 else
 {
-    Console.WriteLine("Это рабочий день.");
+    quadrant = 4;
+}
+{
+Console.WriteLine("Точка находится в четверти: " + quadrant);
 }
